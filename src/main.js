264a316e18c48f2c3bb96e22a441cdb34252a589
Abhinandan Kumar <abhicpr05@gmail.com>
2025-04-13 16:18:12 +0530

    pieces module

--- src/main.js
+++ src/main.js
@@ -11,5 +11,23 @@ const infoDisplay = document.querySelector("#info-display");
 const width = 8;
 
 const startPieces = [
-  
+  rook, knight, bishop, queen, king, bishop, knight, rook, 
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+  rook, knight, bishop, queen, king, bishop, knight, rook,
 ];
+
+function createBoard() {
+  startPieces.forEach((startPieces) => {
+    const square = document.createElement(div);
+    square.classList.add('square');
+    square.classList.add('beige');
+    gameBoard.append(square);
+  })
+}
+
+createBoard();
