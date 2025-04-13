5bcf7427f899489f78ce7770170b678e98ce5ba0
Abhinandan Kumar <abhicpr05@gmail.com>
2025-04-13 15:21:52 +0530

    changing code

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-import './style.css'
+import "./style.css";
 // import javascriptLogo from './javascript.svg'
 // import viteLogo from '/vite.svg'
+
+const gameBoard = document.querySelector("#gameboard");
+
+const playerDisplay = document.querySelector("#player");
+
+const infoDisplay = document.querySelector("#info-display");
+
+const width = 8;
+
+const startPieces = [
+  
+];
